1ad659894fe6405216d35a2738c9715413bff943
chloea3 <102743778+chloea3@users.noreply.github.com>
2023-04-20 17:07:55 -0400

    final commit

--- dev/js/scripts.js
+++ dev/js/scripts.js
@@ -1,1 +1,22 @@
 import { gsap } from "gsap";
+gsap.from("#hero h1, #hero h2", {
+    opacity: 0,
+    duration: 1.5,
+    delay: 2,
+    stagger: 0.6
+});
+
+gsap.from("#hero button", {
+    duration: 2,
+    opacity: 0,
+    scale: 0.5,
+    delay: 3.5,
+    ease: "back.out(1.7)"
+    
+});
+
+gsap.from("header", {
+    opacity: 0,
+    duration: 1.5,
+    delay: 0.5
+  });
