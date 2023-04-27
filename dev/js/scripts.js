cba091a46a082fff203c12037c85acd7df3de8de
chloea3 <102743778+chloea3@users.noreply.github.com>
2023-04-27 15:34:08 -0400

    changed to timeline

--- dev/js/scripts.js
+++ dev/js/scripts.js
@@ -1,22 +1,25 @@
 import { gsap } from "gsap";
-gsap.from("#hero h1, #hero h2", {
-    opacity: 0,
-    duration: 1.5,
-    delay: 2,
-    stagger: 0.6
-});
 
-gsap.from("#hero button", {
-    duration: 2,
-    opacity: 0,
-    scale: 0.5,
-    delay: 3.5,
-    ease: "back.out(1.7)"
-    
-});
+function animateHero() {
+  const tl = gsap.timeline();
+  
+  gsap.set("header", { opacity: 0 });
+  gsap.set("#hero h1, #hero h2", { opacity: 0 });
+  gsap.set("#hero button", { opacity: 0, scale: 0.5 });
+  
+  if (window.matchMedia("(min-width: 768px)").matches) {
+    tl.to({}, 0.1, {}) 
+      .to("header", { opacity: 1, duration: 1.5, delay: 0.2 })
+      .to("#hero h1, #hero h2", { opacity: 1, duration: 1.5, stagger: 0.6 }, "-=0.8")
+      .to("#hero button", { opacity: 1, scale: 1, duration: 2, ease: "back.out(1.7)" }, "-=0.5");
+  } else {
+    tl.to({}, 0.1, {}) 
+      .to("header", { opacity: 1, duration: 1.5, delay: 0.2 })
+      .to("#hero h1, #hero h2", { opacity: 1, duration: 1.5, stagger: 0.6 }, "-=0.8")
+      .to("#hero button", { opacity: 1, duration: 2 }, "-=0.5");
+  }
+}
 
-gsap.from("header", {
-    opacity: 0,
-    duration: 1.5,
-    delay: 0.5
-  });
+requestAnimationFrame(animateHero);
+
+
