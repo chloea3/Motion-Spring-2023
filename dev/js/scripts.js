f66e011e12a82ba5d7e0077ec3dfc33bb95b11a0
chloea3 <102743778+chloea3@users.noreply.github.com>
2023-04-27 20:56:54 -0400

    moves made :D

--- dev/js/scripts.js
+++ dev/js/scripts.js
@@ -3,3 +3,17 @@
 //QUIZ
 
 
+
+import { gsap } from "gsap";
+
+//challenge 1
+gsap.from(".graph", { opacity: 0, duration: 2, ease: "slow(0.7, 0.7, false)" });
+
+gsap.to(".red", { duration: 1, x: 350, ease: "slow(0.7, 0.7, false)" });
+gsap.to(".blue", { duration: 1, x: 0, ease: "slow(0.7, 0.7, false)", delay: 0 });
+//challenge 2
+gsap.to(".red", { duration: 2, y: 0, ease: "slow(0.7, 0.7, false)", delay: 2 });
+gsap.to(".blue", { duration: 2, y: 300, ease: "slow(0.7, 0.7, false)", delay: 1 });
+
+gsap.to(".green", { duration: 4, scaleX: 2, ease: "slow(0.7, 0.7, false)" });
+
