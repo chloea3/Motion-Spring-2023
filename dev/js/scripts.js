a3cdaf1ca8af3a4325f1067a0a95e8a439ed07eb
chloea3 <102743778+chloea3@users.noreply.github.com>
2023-06-08 18:35:57 -0400

    code added

--- dev/js/scripts.js
+++ dev/js/scripts.js
@@ -1,53 +1,9 @@
 import { gsap } from "gsap";
-import { GSDevTools } from "gsap/GSDevTools";
+import { GSDevTools, DrawSVGPlugin } from "gsap/all";
+gsap.registerPlugin(DrawSVGPlugin, GSDevTools);
 
-gsap.registerPlugin(GSDevTools);
-
-gsap.registerPlugin(GSDevTools);
-
-gsap.set("#custom-logo", { visibility: "visible" });
-
-gsap.from("#orange-circle", {
-  opacity: 0,
-  duration: 3,
-  ease: "power2.out"
-});
-
-gsap.from("#pink-circle", {
-  opacity: 0,
-  duration: 3,
-  ease: "power2.out",
-  delay: 1
-});
-
-gsap.from("#white-circle-1", {
-  opacity: 0,
-  duration: 1,
-  ease: "power2.out",
-  delay: 1.5
-});
-
-gsap.from("#white-circle-2", {
-  opacity: 0,
-  duration: 1.5,
-  ease: "power2.out",
-  delay: 1.9
-});
-
-gsap.from("#white-circle-3", {
-  opacity: 0,
-  duration: 1,
-  ease: "power2.out",
-  delay: 2.2
-});
-
-gsap.from("#VER, #BR, #K", {
-  opacity: 0,
-  duration: 1,
-  ease: "power2.out",
-  delay: 2.5
-});
-
-
+const aumioPath = document.querySelector('#aumio');
+gsap.set(aumioPath, { drawSVG: '0%' });
+gsap.to(aumioPath, { duration: 2, drawSVG: '100%', ease: 'power2.out' });
 
 GSDevTools.create();
