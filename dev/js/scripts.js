2bab5543ab9e20c5ebf8c3a491b6da761e08d2a5
chloea3 <102743778+chloea3@users.noreply.github.com>
2023-06-15 18:16:18 -0400

    Update scripts.js

--- dev/js/scripts.js
+++ dev/js/scripts.js
@@ -1,19 +1,15 @@
-// Install the GSAP library and the drawSVG plugin via NPM:
-// npm install gsap gsap-plugin-drawsvg
 
-// Import the required GSAP modules
 import { gsap } from "gsap";
+import { GSDevTools } from "gsap/all";
 import DrawSVGPlugin from "gsap/dist/DrawSVGPlugin";
 
-// Register the DrawSVGPlugin
 gsap.registerPlugin(DrawSVGPlugin);
 
-// Animation code
 gsap.set("#aumio", { visibility: "visible" });
 
-// Animate the drawSVG effect
 gsap.fromTo(
   "#aumio path",
   { drawSVG: "0%" },
   { drawSVG: "100%", duration: 2, ease: "power2.out" }
 );
+
