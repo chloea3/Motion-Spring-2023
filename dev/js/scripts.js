14e61105e9eb2e6feeb3e6c48747db0d6f1df961
chloea3 <102743778+chloea3@users.noreply.github.com>
2023-06-15 17:45:41 -0400

    updates

--- dev/js/scripts.js
+++ dev/js/scripts.js
@@ -1,30 +1,19 @@
-import { gsap } from "gsap";
-import { GSDevTools, DrawSVGPlugin } from "gsap/all";
-gsap.registerPlugin(DrawSVGPlugin, GSDevTools);
+// Install the GSAP library and the drawSVG plugin via NPM:
+// npm install gsap gsap-plugin-drawsvg
 
+// Import the required GSAP modules
+import { gsap } from "gsap";
+import DrawSVGPlugin from "gsap/dist/DrawSVGPlugin";
+
+// Register the DrawSVGPlugin
 gsap.registerPlugin(DrawSVGPlugin);
 
-const mainCircle = document.querySelector("#main-circle");
-const planet = document.querySelector("#planet");
-const circlesGroup = document.querySelector("#circles-group");
-const starX2 = document.querySelector("#star-x-2");
-const starX3 = document.querySelector("#star-x-3");
-const starX4 = document.querySelector("#star-x-4");
-const starX1 = document.querySelector("#star-x-1");
+// Animation code
+gsap.set("#aumio", { visibility: "visible" });
 
-gsap.set([mainCircle, planet, circlesGroup, starX2, starX3, starX4, starX1], { drawSVG: "0%" });
-
-const tl = gsap.timeline({ defaults: { duration: 1, ease: "power2.inOut" } });
-
-tl.to(mainCircle, { drawSVG: "100%" })
-  .to(planet, { drawSVG: "100%" }, "-=0.8")
-  .to(circlesGroup, { drawSVG: "100%" }, "-=0.8")
-  .to(starX2, { drawSVG: "100%" }, "-=0.5")
-  .to(starX3, { drawSVG: "100%" }, "-=0.5")
-  .to(starX4, { drawSVG: "100%" }, "-=0.5")
-  .to(starX1, { drawSVG: "100%" }, "-=0.5");
-
-
-
-
-GSDevTools.create();
+// Animate the drawSVG effect
+gsap.fromTo(
+  "#aumio path",
+  { drawSVG: "0%" },
+  { drawSVG: "100%", duration: 2, ease: "power2.out" }
+);
