237c4845ec6efc8f403e1df5fa0f64c1eba0c4e4
chloea3 <102743778+chloea3@users.noreply.github.com>
2023-06-15 18:03:03 -0400

    final changes

--- dev/js/scripts.js
+++ dev/js/scripts.js
@@ -1,53 +1,56 @@
 import { gsap } from "gsap";
 import { GSDevTools } from "gsap/GSDevTools";
+import { MorphSVGPlugin } from "gsap/all";
 
 gsap.registerPlugin(GSDevTools);
-
-gsap.registerPlugin(GSDevTools);
+gsap.registerPlugin(MorphSVGPlugin);
 
 gsap.set("#custom-logo", { visibility: "visible" });
 
-gsap.from("#orange-circle", {
+const orangeCirclePath = MorphSVGPlugin.convertToPath("#orange-circle");
+const pinkCirclePath = MorphSVGPlugin.convertToPath("#pink-circle");
+const whiteCirclePaths = gsap.utils.toArray("#white-circle-1, #white-circle-2, #white-circle-3").map((circle) => MorphSVGPlugin.convertToPath(circle));
+
+gsap.from(orangeCirclePath, {
   opacity: 0,
   duration: 3,
   ease: "power2.out"
 });
 
-gsap.from("#pink-circle", {
+gsap.from(pinkCirclePath, {
   opacity: 0,
   duration: 3,
   ease: "power2.out",
   delay: 1
 });
 
-gsap.from("#white-circle-1", {
+whiteCirclePaths.forEach((circlePath, index) => {
+  gsap.from(circlePath, {
+    morphSVG: orangeCirclePath,
+    opacity: 0,
+    duration: 1,
+    ease: "power2.out",
+    delay: 2 + (index * 0.3)
+  });
+});
+
+gsap.from("#VER", {
   opacity: 0,
   duration: 1,
   ease: "power2.out",
-  delay: 1.5
+  delay: 2.5 + (whiteCirclePaths.length * 0.3)
 });
-
-gsap.from("#white-circle-2", {
-  opacity: 0,
-  duration: 1.5,
-  ease: "power2.out",
-  delay: 1.9
-});
-
-gsap.from("#white-circle-3", {
+gsap.from("#BR", {
   opacity: 0,
   duration: 1,
   ease: "power2.out",
-  delay: 2.2
+  delay: 2.8 + (whiteCirclePaths.length * 0.3)
 });
-
-gsap.from("#VER, #BR, #K", {
+gsap.from("#K", {
   opacity: 0,
   duration: 1,
   ease: "power2.out",
-  delay: 2.5
+  delay: 3 + (whiteCirclePaths.length * 0.3)
 });
-
-
 
 GSDevTools.create();
